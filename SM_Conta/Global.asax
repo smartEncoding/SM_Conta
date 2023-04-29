04b566951794d00d41068e708715719722297c1e
cbagautier <sebastian.gautier@gmail.com>
2023-04-29 10:31:47 -0400

    Agregar archivos de proyecto.

---
+++ SM_Conta/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SM_Conta.MvcApplication" Language="C#" %>
---
+++ SM_Conta/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SM_Conta.Startup))]
+namespace SM_Conta
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
